f20b0a462ad065994112c024b41a7f32384cd177
yangxin <995249880@qq.com>
2026-06-29 16:13:59 +0800

    2026/06/29：#master_qualcomm_xingtu:星图UI，在应用列表上添加首页同款时间和状态栏。 应用列表背景深色。 屏蔽MAc检测。 更新版本号2.7，修改应用列表右键到最后一个的跳转逻辑

--- app/src/main/java/com/twd/twdlaunchernet/ApplicationActivity.java
+++ app/src/main/java/com/twd/twdlaunchernet/ApplicationActivity.java
@@ -1,13 +1,20 @@
 package com.twd.twdlaunchernet;
 
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.graphics.drawable.Drawable;
+import android.hardware.usb.UsbDevice;
+import android.hardware.usb.UsbManager;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
@@ -18,6 +25,8 @@ import android.view.View;
 import android.widget.Adapter;
 import android.widget.AdapterView;
 import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -29,14 +38,28 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.twd.twdlaunchernet.adapter.ApplicationAdapter;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.Set;
+import java.util.TimeZone;
 
 public class ApplicationActivity extends AppCompatActivity {
+    private TextView tv_time;
+    private TextView tv_day;
+    private ImageView im_wifi;
+    private ImageView im_ble;
+    private ImageView im_usb;
+    private View time_bar;
+    private Handler timerHandler = new Handler();
+    private boolean firstNetwork;
     GridView gridView ;
     ApplicationAdapter adapter;
     SharedPreferences sharedPreferences;
@@ -44,8 +67,14 @@ public class ApplicationActivity extends AppCompatActivity {
     int MyPosition ;
     Context context;
     Utils utils;
+    String ui_theme_code = "Standard";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        if (ui_theme_code.equals("Standard")){
+            this.setTheme(R.style.Theme_Index_Standard);
+        }else if(ui_theme_code.equals("Yameixun")) {
+            this.setTheme(R.style.Theme_Index_Yameixun);
+        }
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_application);
         context = this;
@@ -66,8 +95,37 @@ public class ApplicationActivity extends AppCompatActivity {
         initView();
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        timerHandler.removeCallbacks(updateTimeRunnable);
+        unregisterReceiver(customReceiver);
+        unregisterReceiver(usbReceiver);
+    }
+
+
     private void initView(){
         gridView = findViewById(R.id.gridView);
+        tv_time = findViewById(R.id.tv_time);
+        tv_day = findViewById(R.id.tv_day);
+        im_wifi = findViewById(R.id.im_wifi);
+        im_ble = findViewById(R.id.im_ble);
+        im_usb = findViewById(R.id.im_usb);
+        time_bar = findViewById(R.id.time_bar);
+
+        im_ble.setImageResource(utils.isBluetoothConnected() ? R.drawable.icon_ble_connected : R.drawable.icon_ble);
+        IntentFilter bleFilter = new IntentFilter();
+        bleFilter.addAction(BluetoothDevice.ACTION_ACL_CONNECTED);
+        bleFilter.addAction(BluetoothDevice.ACTION_ACL_DISCONNECTED);
+        registerReceiver(customReceiver,bleFilter);
+
+        im_usb.setImageResource(utils.isUsbPlugged(this) ? R.drawable.icon_usb_connected : R.drawable.icon_usb1);
+        IntentFilter usbFilter = new IntentFilter();
+        usbFilter.addAction(UsbManager.ACTION_USB_DEVICE_ATTACHED);
+        usbFilter.addAction(UsbManager.ACTION_USB_DEVICE_DETACHED);
+        registerReceiver(usbReceiver,usbFilter);
+
+        updateTimeRunnable.run();
         PackageManager pm = getPackageManager();
         Intent intentLauncher = new Intent(Intent.ACTION_MAIN,null);
         intentLauncher.addCategory(Intent.CATEGORY_LAUNCHER);
@@ -97,8 +155,6 @@ public class ApplicationActivity extends AppCompatActivity {
             }
         }
 
-        Log.i("yangxin", "initView: -------初始化已安装应用------");
-        //创建一个迭代器用于遍历installedApps列表
         Iterator<ResolveInfo> iterator = combinedList.iterator();
 
         //遍历installedApps列表，过滤应用
@@ -120,7 +176,6 @@ public class ApplicationActivity extends AppCompatActivity {
         gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Log.i("yangxin", "onItemClick: mode = " + listMode);
                 if(listMode == 1){
                      ResolveInfo app = combinedList.get(position);
                         String packageName = app.activityInfo.packageName;
@@ -175,7 +230,81 @@ public class ApplicationActivity extends AppCompatActivity {
             }
         });
     }
+    private Runnable updateTimeRunnable = new Runnable() {
+        @Override
+        public void run() {
+            if (!firstNetwork){
+                // Log.i(TAG, "run: 判断是否联网");
+                //检查网络连接状态
+                ConnectivityManager connectivityManager =(ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+                NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
 
+                if (networkInfo != null && networkInfo.isConnected()){
+                    // Log.i(TAG, "run: ---------已联网");
+                    //如果设备已连接到网络,从网络获取时间和日期数据
+                    sharedPreferences.edit().putBoolean("firstConnected",true).apply();
+                    firstNetwork = true;
+                    getSystemTime();
+                    im_wifi.setImageResource(R.drawable.icon_wifi_connected);
+                    // 每隔一秒更新一次时间
+                    timerHandler.postDelayed(this, 1000);
+                    return;
+                }else {
+                    // Log.i(TAG, "run: ---------未联网");
+                    // 如果设备未连接到网络，设置时间为--:--，日期不显示
+                    im_wifi.setImageResource(R.drawable.icon_wifi);
+                    tv_time.setText("");
+                    tv_day.setText("");
+                    time_bar.setVisibility(View.GONE);
+                    // 每隔一秒检查网络连接状态
+                    timerHandler.postDelayed(this, 1000);
+                    return;
+                }
+            }
+            //检查网络连接状态
+            ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+            if (networkInfo != null && networkInfo.isConnected()){
+                im_wifi.setImageResource(R.drawable.icon_wifi_connected);
+                getSystemTime();
+            }else {
+                getSystemTime();
+                im_wifi.setImageResource(R.drawable.icon_wifi);
+            }
+            //每隔一秒更新一次时间
+            timerHandler.postDelayed(this,1000);
+        }
+    };
+    private BroadcastReceiver customReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (BluetoothDevice.ACTION_ACL_CONNECTED.equals(action)){
+                //蓝牙设备已连接
+                im_ble.setImageResource(R.drawable.icon_ble_connected);
+            } else if (BluetoothDevice.ACTION_ACL_DISCONNECTED.equals(action)) {
+                //蓝牙设备已断开
+                im_ble.setImageResource(R.drawable.icon_ble);
+            }
+        }
+    };
+
+    private BroadcastReceiver usbReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            UsbDevice device = intent.getParcelableExtra(UsbManager.EXTRA_DEVICE);
+            if (UsbManager.ACTION_USB_DEVICE_ATTACHED.equals(action)){
+                //USB插入
+                Log.d("USB", "USB device attached: " + device.getDeviceName());
+                im_usb.setImageResource(R.drawable.icon_usb_connected);
+            } else  {
+                // USB设备拔出
+                Log.d("USB", "USB device detached: " + device.getDeviceName());
+                im_usb.setImageResource(R.drawable.icon_usb1);
+            }
+        }
+    };
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
         switch (keyCode) {
@@ -213,13 +342,13 @@ public class ApplicationActivity extends AppCompatActivity {
             case KeyEvent.KEYCODE_DPAD_RIGHT:
                 Log.i("yangxin", "onKeyDown: 方向右键被按下 gridView.getSelectedItemPosition() = " + gridView.getSelectedItemPosition());
                 int count = gridView.getAdapter().getCount();
-                if (count % (MyPosition +1) == 0){
+                if (MyPosition == count - 1){
                     Log.i("yangxin", "onKeyDown: 是最后一个，不予处理 ");
                     super.onKeyDown(keyCode, event);
                     break;
                 }
                 Log.i("yangxin", "onKeyDown: 不是最后一个，处理position ");
-                gridView.setSelection(gridView.getSelectedItemPosition() + 1);
+                gridView.setSelection(MyPosition + 1);
                 break;
             case KeyEvent.KEYCODE_DPAD_LEFT:
                 Log.i("yangxin", "onKeyDown: 方向左键被按下 gridView.getSelectedItemPosition() = " + gridView.getSelectedItemPosition());
@@ -228,7 +357,7 @@ public class ApplicationActivity extends AppCompatActivity {
                     super.onKeyDown(keyCode, event);
                     break;
                 }
-                gridView.setSelection(gridView.getSelectedItemPosition() - 1);
+                gridView.setSelection(MyPosition  - 1);
                 Log.i("yangxin", "onKeyDown: 不是第一个，处理position ");
                 break;
         }
@@ -273,5 +402,34 @@ public class ApplicationActivity extends AppCompatActivity {
         Intent intent = new Intent(Intent.ACTION_DELETE, uri);
         startActivity(intent);
         //Utils.execCommand(packageName);
+    }
+    private void getSystemTime(){
+        //获取当前时间和日期
+        Calendar calendar = Calendar.getInstance();
+        Date currentDate = calendar.getTime();
+
+
+        //设置日期的格式
+        TimeZone timeZone = calendar.getTimeZone();
+        DateFormat dateFormat;
+        if ("Asia/Shanghai".equals(timeZone.getID())){
+            dateFormat = new SimpleDateFormat("yyyy/MM/dd");
+        }else {
+            dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        }
+        String formatterDate = dateFormat.format(currentDate);
+
+        String dayOfWeek = new SimpleDateFormat("EEEE", Locale.getDefault()).format(currentDate);
+        formatterDate = dayOfWeek+"\n"+formatterDate;
+
+        String timeFormatString = Utils.getTimeFormat(this);
+        //设置时间的格式
+        DateFormat timeFormat = new SimpleDateFormat(timeFormatString);
+        String formatterTime = timeFormat.format(currentDate);
+
+        //在TextView上更新日期和时间
+        tv_day.setText(formatterDate);
+        tv_time.setText(formatterTime);
+        time_bar.setVisibility(View.VISIBLE);
     }
 }
